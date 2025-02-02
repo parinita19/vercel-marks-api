babb332833f3ed9211f1596fd8548210c2506cdf
parinita19 <23f2003537@ds.study.iitm.ac.in>
2025-02-02 22:02:03 +0530

    modified

--- index.py
+++ index.py
@@ -13,10 +13,9 @@ json_path = os.path.join(current_dir, 'marks.json')
 # Load marks from JSON file
 with open(json_path, 'r') as f:
     marks_data = json.load(f)
-    # Convert list to dictionary for faster lookups
     marks_db = {item['name']: item['marks'] for item in marks_data}
 
-@app.route('/api', methods=['GET'])
+@app.route('/', methods=['GET'])
 def get_marks():
     names = request.args.getlist('name')
     marks = [marks_db.get(name, 0) for name in names]
